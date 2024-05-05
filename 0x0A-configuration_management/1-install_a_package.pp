46ac3665cc7c5c46ad6e85dc96ce696c7a9e50f1
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-05-05 09:50:20 +0100

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# Using Puppet to install flask from pip3
-package { 'flask':
+# 1-install_a_package.pp
+
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Define a package resource for Flask, specifying the provider and version
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
